af4476f0e721a5976fd15f4b758f452227a0aae1
CFitzy <cathryn.mae.fitzpatrick@gmail.com>
2025-07-21 12:38:18 +0100

    setup wiz basics

---
+++ docs/Turtle commands.md
@@ -0,0 +1,81 @@
+# Turtle basics
+
+## Turtle Commands
+* l*n* - left wheel n steps (4096 per wheel rotation)
+* r*n* - right wheel n steps (4096 per wheel rotation)
+* f*n* - forward n steps (-n backward)
+* t*n* - turn n steps (+ clockwise, -anticlockwise)
+* F*mm* - forward F milimeters (-mm backward)
+* T*d* - turn d degrees (+ clockwise, -anticlockwise)
+* a*n* - acceleration number of steps between slow and fast speed
+* o - motors off
+* u - pen up to max height
+* d - pen down to min height
+* U*d* - pen up to d height (0<d<=1, 0=u height, 1=d height). Note this does not persist after power down. Use s5 setting to set power up default.
+* U - pen up to previously set height
+* D*d* - pen down to d height (0<d<=1, 0=u height, 1=d height). Note this does not persist after power down. Use s6 setting to set power up default.
+* D - pen down to previously set height
+* =text - display text on OLED screen
+* x*d* run step streaming program d
+
+Note that the lower case commands f,t do not ina;ude backlash compensation and directly drive the motors. The normal commands (capital letters) do include backlash compensation (to reduce motor gear slop effects when motors change direction) if non zero compensation values are set.
+
+## Settings
+
+* s1*n* - set motor speed in nS per step (1000 default)
+* s2*d* - set left wheel diameter in mm
+* s3*d* - set right wheel diameter in mm
+* s4*d* - set wheel spacing (axle length) in mm
+* s5*d* - set default user pen up position (0.1 - 1.0)
+* s6*d* - set default user pen down position (0.0 - 1.0)
+* s7*i* - set left wheel backlash (in motor steps)
+* s8*i* - set right wheel backlash (in motor steps)
+
+###Permanent settings are
+
+s2 wheelDiameterL; // wheel diameter in mm x 10
+s3 wheelDiameterR; // wheel diameter in mm x 10
+s4 wheelSpacing; // wheel spacing ('axle length') x10
+s5 penUp; // pen up position (U command)
+s6 penDown; // pen down position (D command)
+s7 left wheel backlash
+s8 roght wheel backlash
+
+#Java code
+
+the following only applied to the  application which intercepts these commands to perform special functions for the turtle. e.g. loading/creating a file that is then used to send the commands to the turtle.
+
+
+## File commands 
+
+### send commands (ie. not step mode)
+
+* tc filename - send command file to turtle 
+* tcsvg filename - send lines and turns to turtle 
+
+### stream steps (ie. wheel steps)
+* svg filename - stream file as lines and arc'd beziers
+* svgl filename - stream as lines and turns (segmented beziers)
+* svga filename - stream steps only arcs (except moves)
+
+
+## Settings only for the java svg code 
+These are not part of the Turtle firmware, but are intercepted by the Java terminal application to set values used in the wheel step streaming protocol intended for drawing very complex curve based designs.
+
+* j3*n* - set STEP\_DIFF for extra steps if L/R diff
+* j4*n* - set EXTRA\_STEPS steps to add if STEP\_DIFF occurs
+
+## EEPROM
+* get - applies EEPROM settings and sends to the serial interface
+* save - saves the current settings to the EEPROM so they will be restored on power up. This is designed for initial configuration of a new turtle or after mechanical changes. Should not be needed for 'everyday' use. ge5 
+
+# Java code notes
+The `CommandMaker` class main method creates the svg output for wheel paths (as a visualisation)
+
+The Turtle class main method is the entry point for the terminal based Turtle controller
+
+After updating the Turtle firmware issue a `tc cfg` command to transfer configs to the turtle and save in EEPROM. 
+
+The `svg` command series streams motor step commands to the turtle. The configurations are irrelevant here except for the `motorSpeedFast` which must match the java`Turtle.STEP_TIME`. The Java code the generates the pattern must therefore have the correct wheel and axle lengths, as well as any backlash required. This is all done in the PatternMaker class using `stepsLinesAndCurveArcs` and related methods to convert an svg file to steps. 
+
+Since the PatternMaker needs the correct config for the turtle to be used it is run from `CommandMaker.main` which loads cfgJ.txt and then generates steps and visualisations for the input svg file.
--- src/setup_wizard.py
+++ src/setup_wizard.py
@@ -7,12 +7,16 @@ Created on Wed Jul  9 11:06:05 2025
 import tkinter as tk
 import customtkinter as ctk
 import re
+import time
+import math
 
 class setup_wizard():
     def __init__(self, port_manager):
         self.circle_steps = 4096
         self.port_manager = port_manager
         self.settings = {}
+        self.title_font = ("Roboto", 14)
+        self.paragraph_font = ("Roboto", 12)
         
     def get_settings(self, settings):
         #'wheelL 53.18\r\nwheelR 53.18\r\nAxle 79.04\r\nPenU  0.40\r\nPenD  0.30\r\nBacklashL 0\r\nBacklashR 0\r\n'
@@ -26,10 +30,7 @@ class setup_wizard():
         print(self.settings)
         
     def setup_wizard(self):
-        #get saved values
-        self.get_settings(self.port_manager.get_settings())
-        
-        
+
         self.wizard_pop_up = ctk.CTkToplevel()
         self.wizard_pop_up.grab_set()           # Stop other window interaction
         self.wizard_pop_up.focus_force()        # Set input focus to the popup
@@ -38,21 +39,23 @@ class setup_wizard():
         
         self.frame = ctk.CTkFrame(self.wizard_pop_up, fg_color="transparent")
         self.frame.pack(expand=True, fill=ctk.BOTH)
-        ctk.CTkLabel(self.frame, text="Setup turtlebot dimensions").pack(pady=1)
+        ctk.CTkLabel(self.frame, text="Setup turtlebot dimensions", font=self.title_font).pack(pady=1)
         
         
         if not self.port_manager.allow_writing:
             ctk.CTkLabel(self.frame, text="Connect the turtlebot before setting it up").pack(pady=10)
             ctk.CTkButton(self.frame, text="Close", command=self.wizard_pop_up.destroy).pack(side=ctk.BOTTOM, pady=3)
         else:
-            ctk.CTkLabel(self.frame, text="You will need: \n*A ruler\n*A big piece of paper (A3 or bigger recommended)").pack(pady=10)
+            #get saved values
+            self.get_settings(self.port_manager.get_settings())
+            ctk.CTkLabel(self.frame, text="You will need: \n*A ruler with mm\n*A big piece of paper (A3 or bigger recommended)").pack(pady=10)
             ctk.CTkButton(self.frame, text="Start", command=self.check_backlash_start).pack(side=ctk.BOTTOM, pady=3)
         
     def check_backlash_start(self):
         for w in self.frame.winfo_children():
             w.destroy()
             
-        ctk.CTkLabel(self.frame, text="Setup turtlebot: Backlash").pack(pady=1)
+        ctk.CTkLabel(self.frame, text="Setup turtlebot: Backlash", font=self.title_font).pack(pady=1)
         ctk.CTkLabel(self.frame, text="Press start for the turtlebot to draw a line").pack(pady=10)
         
         ctk.CTkButton(self.frame, text="Start", command=self.check_backlash_main).pack(side=ctk.BOTTOM)
@@ -63,10 +66,16 @@ class setup_wizard():
         for w in self.frame.winfo_children():
             w.destroy()
             
+        self.port_manager.send_command("s7 0")
+        self.port_manager.send_command("s8 0")    
         self.port_manager.send_command("F-50")
+        
             
-        ctk.CTkLabel(self.frame, text="Setup turtlebot: Backlash").pack(pady=1)
+        ctk.CTkLabel(self.frame, text="Setup turtlebot: Backlash", font=self.title_font).pack(pady=1)
         ctk.CTkLabel(self.frame, text="Press forward to move the turtlebot forward, if you see it move press next").pack(pady=10)
+        
+        self.backlash_label = ctk.CTkLabel(self.frame, text=self.current)
+        self.backlash_label.pack()
         
         self.button_frame = ctk.CTkFrame(self.frame, fg_color="transparent")
         self.button_frame.pack(side=ctk.BOTTOM)
@@ -78,9 +87,11 @@ class setup_wizard():
         
         
     def backlash_forward(self):
-        self.current = self.current + self.increment
+        print(self.increment)
+        self.current = round(self.current + self.increment,2)
         self.port_manager.send_command("F"+str(self.increment))
         print("F:", self.current, self.increment)
+        self.backlash_label.configure(text=self.current)
         
     def backlash_last_increment(self):
         print("BB", self.current, self.increment)
@@ -97,63 +108,226 @@ class setup_wizard():
     def backlash_next_increment(self):
         if self.increment/5<0.01:
             #set backlash
+            backlash = self.current-self.increment
             self.port_manager.send_command("o")
-            self.port_manager.send_command("s7 "+str(self.current-self.increment))
-            self.port_manager.send_command("s8 "+str(self.current-self.increment))
-            self.check_wheel_diameter_start()
+            self.port_manager.send_command("s7 "+str(backlash))
+            self.port_manager.send_command("s8 "+str(backlash))
+            self.settings.update({"BacklashL": backlash})
+            self.settings.update({"BacklashR": backlash})
+            print(self.settings)
+            self.check_wheel_diameter()
         
         else:
-            print(self.back_button in self.frame.winfo_children())
+            #print(self.back_button in self.frame.winfo_children())
             
-            self.port_manager.send_command("F-"+str(self.current))
-            self.current = self.current - self.increment
+            if self.current>0:
+                self.port_manager.send_command("F-"+str(self.current))
+                self.current = self.current - self.increment
+            else:
+                self.current = self.current - self.increment
+                self.port_manager.send_command("F"+str(self.current))
+            
             self.increment = self.increment/5
             print("N:", self.current, self.increment)
             
             if self.increment <10:
                 self.back_button.configure(state=ctk.NORMAL)
+            self.backlash_label.configure(text=self.current)
             
-        
-    
-        
-        
     
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    def check_wheel_diameter_start(self):
+    def check_wheel_diameter(self):
         for w in self.frame.winfo_children():
             w.destroy()
             
-        ctk.CTkLabel(self.frame, text="Setup turtlebot wheels").pack(pady=1)
-        ctk.CTkLabel(self.frame, text="Press start for the turtlebot to draw a line").pack(pady=10)
         
-        ctk.CTkButton(self.frame, text="Start", command=self.check_wheel_diameter_main).pack(side=ctk.BOTTOM)
-        
-    def check_wheel_diameter_main(self):
-        for w in self.frame.winfo_children():
-            w.destroy()
             
+        ctk.CTkLabel(self.frame, text="Setup turtlebot wheels", font=self.title_font).pack(pady=1)
+        ctk.CTkLabel(self.frame, text="Press Draw to draw a line. Measure the length then input the value").pack(pady=10)
+        
+        self.bottom_frame = ctk.CTkFrame(self.frame, fg_color="transparent")
+        self.bottom_frame.pack()
+        
+        self.draw_diameter_button()
+        
+        
+        
+    def draw_diameter_button(self):
+        for w in self.bottom_frame.winfo_children():
+            w.destroy()
+        
+        self.wheel_draw_button = ctk.CTkButton(self.bottom_frame, text="Draw", command=self.draw_for_diameter)
+        self.wheel_draw_button.pack()
+        
+    def draw_for_diameter(self):
+        self.wheel_draw_button.configure(text="Drawing")
+        #Wheel Diameter simple calibration test 
+        # slow down
+        self.port_manager.send_command("s1 4000")
+        # take up backlash
+        
+        self.port_manager.send_command("F 10")
+        # lower pen
         self.port_manager.send_command("D")
+        # move 30cm
         self.port_manager.send_command("F300")
         self.port_manager.send_command("U")
+        # reset motor speed (uS per step, 1100 fastest @ 7Volt)
+        self.port_manager.send_command("s1 1100")
         self.port_manager.send_command("o")
-            
-        ctk.CTkLabel(self.frame, text="Setup turtlebot wheels").pack(pady=1)
-        ctk.CTkLabel(self.frame, text="Measure the line").pack(pady=10)
+        time.sleep(15)
         
         #we assume it will be straight "enough" as the wheels are most likely printed at the same time so have been effected equally
+        for w in self.bottom_frame.winfo_children():
+            w.destroy()
+        length_frame = ctk.CTkFrame(self.bottom_frame, fg_color="transparent")
+        length_frame.pack()
+        ctk.CTkLabel(length_frame, text="Length:", font=("Roboto", 12)).pack(side=ctk.LEFT)
+        self.length_input = ctk.CTkTextbox(length_frame, height=50)
+        self.length_input.pack(side=ctk.LEFT, padx=2)
+        ctk.CTkLabel(length_frame, text="mm").pack(side=ctk.LEFT)
+        ctk.CTkButton(self.bottom_frame, text="Next", command=self.diameter_length_check).pack(side=ctk.BOTTOM)
         
-        ctk.CTkButton(self.frame, text="300mm").pack(side=ctk.BOTTOM)
-        ctk.CTkButton(self.frame, text="Not 300mm").pack(side=ctk.BOTTOM)
+        
+    def diameter_length_check(self):
+        #get length
+        length = self.length_input.get("0.0",ctk.END)                         #input validation    
+        print(length)
+        if float(length) == 300:
+            self.check_axle_length()
+        else:
+            self.calculate_wheel_diameter(length)
+            self.draw_diameter_button()
+            
+    def calculate_wheel_diameter(self, length):
+        #we have required distance dr and travelled distance dt
+        #error distance de = dt-dr (positive if travels too far)
+
+        # circumference of wheel, Wc= PI*Wd
+        # wheel rotations = dr/Wc
+        # percentage error = dt/dr
+        #since wheel diameter is proportional to circumference we change the wheel diameter by a proportional amount
+        #adjusted wheel diameter = dt/dr*Wd
+        
+        #get expected wheel diameter
+        expected_diameter = self.settings.get("wheelL")
+        
+        actual_diameter = (float(length)/300)*float(expected_diameter)
+        
+        #set values for diameters
+        self.port_manager.send_command("s2 "+str(actual_diameter))
+        self.port_manager.send_command("s3 "+str(actual_diameter))
+        self.settings.update({"wheelL": actual_diameter})
+        self.settings.update({"wheelR": actual_diameter})
+        print(self.settings)
+        
+        
+    def check_axle_length(self):
+        for w in self.frame.winfo_children():
+            w.destroy()
+            
+        
+            
+        ctk.CTkLabel(self.frame, text="Setup turtlebot axle", font=self.title_font).pack(pady=1)
+        ctk.CTkLabel(self.frame, text="Press Draw to draw two circles. Measure the distance of overlap/gap then input the values. If a gap put \"-\" before the number").pack(pady=10)
+        
+        ctk.CTkButton(self.frame, text="Draw", command=self.draw_for_axle).pack()
+        
+        #we assume it will be straight "enough" as the wheels are most likely printed at the same time so have been effected equally
+        length_frame = ctk.CTkFrame(self.frame, fg_color="transparent")
+        length_frame.pack()
+        ctk.CTkLabel(length_frame, text="Length:", font=("Roboto", 12)).pack(side=ctk.LEFT)
+        self.length_input = ctk.CTkTextbox(length_frame, height=50)
+        self.length_input.pack(side=ctk.LEFT, padx=2)
+        ctk.CTkLabel(length_frame, text="Length2:", font=("Roboto", 12)).pack(side=ctk.LEFT)
+        self.length_input2 = ctk.CTkTextbox(length_frame, height=50)
+        self.length_input2.pack(side=ctk.LEFT, padx=2)
+        ctk.CTkLabel(length_frame, text="mm").pack(side=ctk.LEFT)
+        ctk.CTkButton(self.frame, text="Done").pack(side=ctk.BOTTOM)
+        
+    def draw_for_axle(self):
+        #Axle length simple calibration test turn using only one wheel, if the wheel is calibrated any error will be due to the wheel spacing
+
+        #   slow down
+        self.port_manager.send_command("s1 3000")
+        # remove backlash compensation settings
+        self.port_manager.send_command("s7 0")
+        self.port_manager.send_command("s8 0")
+
+    # if Ad is Axle length (diameter) and Wd is wheel diameter
+    # then circumference is Ac = 2*Ad*PI
+    # Wheel circumference Wc = Wd*PI 
+    # so we need Ac/Wc rotation of the wheel
+    # or (with 4096 steps per rotation) Ac/Wc*4096
+    # so Ws = Ad/Wd
+
+        self.port_manager.send_command("U")
+    # Take up backlash
+        self.port_manager.send_command("l 200")
+
+        self.port_manager.send_command("D")
+
+    #turn for a circle: ((2*axle)/wheel diameter)*steps for rotation
+        self.port_manager.send_command("l "+str(round(((2*float(self.settings.get("Axle")))/float(self.settings.get("wheelL")))*4096),5))
+
+        self.port_manager.send_command("U")
+        
+        self.port_manager.send_command("F 30")
+
+    #Take up backlash
+        self.port_manager.send_command("r 200")
+
+        self.port_manager.send_command("D")
+    #r ((2*79.6)/53.02)*4096
+    #r 12299
+        self.port_manager.send_command("r "+str(round(((2*float(self.settings.get("Axle")))/float(self.settings.get("wheelR")))*4096),5))
+        self.port_manager.send_command("U")
+
+    # turn off motors
+        self.port_manager.send_command("F 30")
+        self.port_manager.send_command("s1 1100")
+        self.port_manager.send_command("o")
+
+    def calculate_axle_length(self):
+    # the circle ends either overlap or have a gap.
+    # if they overlap reduce the axle length setting by 2mm until there is a gap 
+    # (a gap is easier to measure)
+    # to adjust axle take the average axle circumference error (Ace) of the two directions 
+    # using the set Ad (axle length)
+    # axle circumference is Ac = 2*Ad*PI
+    # the percentage error Ace = (Ac+Ace)/Ac
+    # so the axle is longer than the code thinks (calculated c is too small)
+    # new axle length is Ace*Ad.
+
+    # axle*(((2*axle*pi)-overlap)/(2*axle*pi))      
+    #+gap for gap and -overlap
+    #simplifies to axel-(overlap/2*pi) or axle+(gap/2*pi)
+        print("calc")
+        self.gap=False #remove
+        #get length
+        length = float(self.length_input.get("0.0",ctk.END))
+        #get length2
+        length2 = float(self.length_input2.get("0.0",ctk.END))
+    
+        avg_len = (length+length2)/2
+    
+        #get expected wheel diameter
+        expected_axle = self.settings.get("Axle")
+    
+        actual_axle = round(expected_axle -(avg_len/2*math.pi), 3)
+    
+        #set values for axle
+        self.settings.update({"Axle": actual_axle})
+        print(self.settings)
+    
+    def save_all(self):
+        self.port_manager.send_command("s2 "+str(self.settings.get("wheelL")))
+        self.port_manager.send_command("s3 "+str(self.settings.get("wheelR")))
+        self.port_manager.send_command("s4 "+str(self.settings.get("Axle")))
+        
+        self.port_manager.send_command("s7 "+str(self.settings.get("BacklashL")))
+        self.port_manager.send_command("s8 "+str(self.settings.get("BacklashR")))
+        
         
     
